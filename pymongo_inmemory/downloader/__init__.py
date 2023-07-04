e4429b7cfbc6f3f74336b98027a6fc6281e815ad
Ertugrul Karademir <ekarademir@gmail.com>
2023-07-04 23:46:51 +0100

    Ertugrul/refactor (#71)

--- pymongo_inmemory/downloader/__init__.py
+++ pymongo_inmemory/downloader/__init__.py
@@ -2,20 +2,15 @@ import glob
 import distutils
 import zipfile
 import logging
-import os
 from os import path
-import platform
 import shutil
 import tarfile
 import tempfile
 import urllib.request as request
 from urllib.error import HTTPError
 
-from .._utils import conf
-from ._urls import best_url
+from ..context import Context
 
-
-CACHE_FOLDER = path.join(path.dirname(__file__), "..", ".cache")
 
 logger = logging.getLogger("PYMONGOIM_DOWNLOADER")
 
@@ -32,47 +27,25 @@ class InvalidDownloadedFile(Exception):
     pass
 
 
-def _mkdir_ifnot_exist(*folders):
-    current_path = path.join(folders[0])
-    if not path.isdir(current_path):
-        os.mkdir(current_path)
-    for x in folders[1:]:
-        current_path = path.join(current_path, x)
-        if not path.isdir(current_path):
-            os.mkdir(current_path)
-    return current_path
-
-
-def _download_folder():
-    return conf("download_folder", _mkdir_ifnot_exist(CACHE_FOLDER, "download"))
-
-
-def _extract_folder():
-    return conf("extract_folder", _mkdir_ifnot_exist(CACHE_FOLDER, "extract"))
-
-
 def _dl_reporter(blocknum, block_size, total_size):
     percent_dled = blocknum * block_size / total_size * 100
     size_dlded = blocknum * block_size / 1024 / 1024  # MBs
     total_size = total_size / 1024 / 1024  # MBs
     if 0 <= percent_dled % 10 <= 0.01:
-        logger.info("{:.0f} % ({:.0f} MiB of {:.0f} MiB)".format(
-            percent_dled, size_dlded, total_size)
+        logger.info(
+            "{:.0f} % ({:.0f} MiB of {:.0f} MiB)".format(
+                percent_dled, size_dlded, total_size
+            )
         )
 
 
 def _download_file(dl_url, destination_file):
-    dl_folder = _download_folder()
-
-    if not path.isdir(dl_folder):
-        logger.debug("Download folder doesn't exist, creating it.")
-        os.mkdir(dl_folder)
-
     if path.isfile(destination_file):
-        logger.debug((
-            "There is already a downloaded file {}, "
-            "skipping download"
-        ).format(destination_file))
+        logger.debug(
+            ("There is already a downloaded file {}, " "skipping download").format(
+                destination_file
+            )
+        )
         return
 
     with tempfile.NamedTemporaryFile(delete=False) as temp:
@@ -80,56 +53,53 @@ def _download_file(dl_url, destination_file):
         try:
             request.urlretrieve(dl_url, filename=temp.name, reporthook=_dl_reporter)
         except HTTPError:
-            raise CantDownload((
-                "Can't download {url}, "
-                "make sure MongoDB provides it. "
-                "Possibly the version is not provided for the operating system."
-            ).format(url=dl_url))
+            raise CantDownload(
+                (
+                    "Can't download {url}, "
+                    "make sure MongoDB provides it. "
+                    "Possibly the version is not provided for the operating system."
+                ).format(url=dl_url)
+            )
 
         logger.debug("Finished download.")
         shutil.copyfile(temp.name, destination_file)
         logger.debug("Copied file to {}".format(destination_file))
 
 
-def _extract(archive_file):
+def _extract(archive_file, extracted_folder):
     logger.info("Extracting from the archive, {}".format(archive_file))
-    extract_folder = _extract_folder()
 
     if tarfile.is_tarfile(archive_file):
-        _extract_tar(archive_file, extract_folder)
+        _extract_tar(archive_file, extracted_folder)
     elif zipfile.is_zipfile(archive_file):
-        _extract_zip(archive_file, extract_folder)
+        _extract_zip(archive_file, extracted_folder)
     else:
         raise InvalidDownloadedFile("Expecting either a .tar or a .zip file.")
 
 
-def _extract_tar(tar_file, extract_folder):
+def _extract_tar(tar_file, extracted_folder):
     with tarfile.open(tar_file, "r") as t:
         logger.info("Starting extraction.")
         for f in t.getnames():
-            logger.debug("Extracting {} to {}".format(f, extract_folder))
-            t.extract(f, extract_folder)
+            logger.debug("Extracting {} to {}".format(f, extracted_folder))
+            t.extract(f, extracted_folder)
             logger.debug("Done extracting {}".format(f))
         logger.info("Extractiong finished.")
 
 
-def _extract_zip(zip_file, extract_folder):
+def _extract_zip(zip_file, extracted_folder):
     with zipfile.ZipFile(zip_file) as z:
         logger.info("Starting extraction.")
         for f in z.namelist():
-            logger.debug("Extracting {} to {}".format(f, extract_folder))
-            z.extract(f, extract_folder)
+            logger.debug("Extracting {} to {}".format(f, extracted_folder))
+            z.extract(f, extracted_folder)
             logger.debug("Done extracting {}".format(f))
         logger.info("Extractiong finished.")
 
 
-def _collect_archive_name(url):
-    return url.split("/")[-1]
-
-
-def _get_mongod():
+def _get_mongod(extracted_folder):
     for binfile_path in glob.iglob(
-        path.join(_extract_folder(), f"**/bin/*"), recursive=True
+        path.join(extracted_folder, "**", "bin", "*"), recursive=True
     ):
         binfile_name = path.basename(binfile_path)
         try:
@@ -140,88 +110,19 @@ def _get_mongod():
             return binfile_path
 
 
-def download(os_name=None, version=None, os_ver=None, ignore_cache=False):
-    """Download MongoDB binaries.
-    Available versions are collected form this URL:
-    https://www.mongodb.com/download-center/community/releases
-    and this one:
-    https://www.mongodb.com/download-center/community/releases/archive
-
-    Parameters
-    ----------
-    os_name: str
-        If `None`, then it'll try to determine based on `platform.system()`, if can't
-        determined `OperatingSystemNotFound` will be raised
-    version: str
-        MongoDB version
-    os_ver: str
-        Operating system version, if the OS has several versions
-    ignore_cache: bool
-        Download MongoDB, even if there is already one in the cache
-
-    Raises
-    ------
-    OperatingSystemNotFound: If OS is not MacOS, Windows or Linux variant.
-    """
-    if version is None:
-        version = conf("mongo_version")
-
-    if os_name is None:
-        os_name = conf("operating_system")
-        if os_name is None:
-            _mapping = {"Darwin": "osx", "Linux": "linux", "Windows": "windows"}
-            os_name = _mapping.get(platform.system())
-            if os_name is None:
-                raise OperatingSystemNotFound("Can't determine operating system.")
-
-    if os_name == "linux":
-        logger.warning((
-            "Starting from MongoDB 4.0.23 "
-            "there isn't a generic Linux version of MongoDB"
-            ))
-
-    if os_ver is None:
-        os_ver = conf("os_version")
-
-    dl_url = conf("download_url", best_url(
-        os_name,
-        version=version,
-        os_ver=os_ver
-    ))
+def download(pim_context: Context):
+    dl_url = pim_context.download_url
+    should_ignore_cache = pim_context.ignore_cache
 
     logger.debug("Downloading MongoD from {}".format(dl_url))
-    dl_folder = _download_folder()
-    archive_file = path.join(dl_folder, _collect_archive_name(dl_url))
-
-    should_ignore_cache = bool(conf("ignore_cache", ignore_cache))
+    archive_file = path.join(pim_context.archive_folder, "archive")
 
     if should_ignore_cache or not path.isfile(archive_file):
         logger.info("Archive file is not found, {}".format(archive_file))
         _download_file(dl_url, archive_file)
-        _extract(archive_file)
+        _extract(archive_file, pim_context.extracted_folder)
 
-        if os_name == "linux":
-            dl_url = "https://downloads.mongodb.com/compass/mongosh-1.8.0-linux-x64.tgz"
-        else:
-            dl_url = "https://downloads.mongodb.com/compass/mongosh-1.8.0-win32-x64.zip"
-        archive_file_mongo = path.join(dl_folder, _collect_archive_name(dl_url))
-        mongod_folder = _get_bin_from_zip_file("mongodb*")
-        _download_file(dl_url, archive_file_mongo)
-        _extract(archive_file_mongo)
-        mongo_folder = _get_bin_from_zip_file("mongosh*")
-        if os_name == "linux":
-            os.rename(os.path.join(mongo_folder, "mongosh"), os.path.join(mongo_folder, "mongo"))
-        else:
-            os.rename(
-                os.path.join(mongo_folder, "mongosh.exe"), os.path.join(mongo_folder, "mongo.exe")
-            )
-        import distutils
+    if _get_mongod(pim_context.extracted_folder) is None:
+        _extract(archive_file, pim_context.extracted_folder)
 
-        distutils.dir_util.copy_tree(mongo_folder, mongod_folder)
-    return path.dirname(_get_mongod())
-
-def _get_bin_from_zip_file(folder_name):
-
-    res = os.path.join(_extract_folder(), folder_name, "bin")
-    res = glob.glob(res)[0]
-    return os.path.normpath(res)
+    return path.dirname(_get_mongod(pim_context.extracted_folder))
